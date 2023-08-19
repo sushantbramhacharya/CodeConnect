2f213336f81dfa7d5711e0da61c905fefed2258c
Amit Maharjan <amitmaharjan2002@gmail.com>
2023-08-19 22:27:02 +0545

    changes in connected users info time removed search removed

--- Frontend/c_c_messenger/src/App.js
+++ Frontend/c_c_messenger/src/App.js
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 
 function App() {
 
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({
+    connected_users:[]
+  });
 
   useEffect(()=>{
     fetch("http://localhost/Codeconnect/messages/index.php",{
@@ -24,8 +26,9 @@ function App() {
 
   return (<div className="container">
     <h1 className="header-text">WELCOME TO CODE CONNECT MESSENGER,{user.name} <a href="http://localhost/codeconnect">BACK TO Code Connect</a> </h1>
+    {console.log(user.connected_users)}
     <div className="row">
-      <Sidebar/>
+      <Sidebar connected_users={user.connected_users} />
       <Chat/>
     </div>
   </div>)
--- Frontend/c_c_messenger/src/components/Sidebar.js
+++ Frontend/c_c_messenger/src/components/Sidebar.js
@@ -2,52 +2,24 @@ import { Component } from 'react';
 class Sidebar extends Component {
     render()
     {
+      const {connected_users}=this.props;
+      console.log(connected_users);
         return<section className="discussions">
-        <div className="discussion search">
-          <div className="searchbar">
-            <i className="fa fa-search" aria-hidden="true"></i>
-            <input type="text" placeholder="Search..."></input>
-          </div>
-        </div>
-        <div className="discussion message-active">
+        {connected_users.map((user_info, index)=>
+        {
+          return <div key={index} className="discussion message-active">
           <div className="photo" style={
             {backgroundImage: "url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)"}
             }>
             <div className="online"></div>
           </div>
           <div className="desc-contact">
-            <p className="name">Megan Leib</p>
+            <p className="name">{user_info.name}</p>
             <p className="message">9 pm at the bar if possible 😳</p>
           </div>
-          <div className="timer">12 sec</div>
-        </div>
-
-        <div className="discussion">
-          <div className="photo" style={
-            {backgroundImage:"url(https://i.pinimg.com/originals/a9/26/52/a926525d966c9479c18d3b4f8e64b434.jpg)"}}>
-            <div className="online"></div>
-          </div>
-          <div className="desc-contact">
-            <p className="name">Dave Corlew</p>
-            <p className="message">Let's meet for a coffee or something today ?</p>
-          </div>
-          <div className="timer">3 min</div>
-        </div>
-
+        </div>;
+        })}
         
-
-        
-
-        <div className="discussion">
-          <div className="photo" style={
-            {backgroundImage:"url(https://images.unsplash.com/photo-1553514029-1318c9127859?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80)"}}>
-          </div>
-          <div className="desc-contact">
-            <p className="name">Elsie Amador</p>
-            <p className="message">What the f**k is going on ?</p>
-          </div>
-          <div className="timer">1 day</div>
-        </div>
       </section>;
     }}
     export default Sidebar;
